3571d55998ffea3413053ec3e0168c8d5d7c02f1
stu9arkin <stusnout@gmail.com>
2022-06-06 11:59:44 +0100

    Removed duplicates

--- setupScripts/getSlots.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as sql
-
-conn = sql.connect("database.db")
-
-query = conn.execute("SELECT * FROM Day;")
-fetchQuery = query.fetchall()
-
-print(fetchQuery)
--- setupScripts/getTeachers.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as sql
-
-conn = sql.connect("database.db")
-
-query = conn.execute("SELECT * FROM Teachers;")
-fetchQuery = query.fetchall()
-
-print(fetchQuery)
